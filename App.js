59badd3ddc99b9d28ea2f02fa97b7cabe60fd66c
Omar Mendoza <90891308+omar1007@users.noreply.github.com>
2022-05-18 02:11:16 -0500

    first view

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { useState } from 'react/cjs/react.production.min';
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text style={styles.MainTitle}>HELLO WORLD</Text>
+        <Text style={styles.textview}>Bienvenid@s a nuestra aplicación. 
+        Aquí encontraras el mejor catálogo de series y peliculas que se
+        encuentran dentro de tus servicios de streaming favoritos.</Text>
+        <StatusBar style="auto" />
+        <Button 
+          title={'CONTINUAR'} 
+          style={styles.boton}
+          onPress={()=> alert('HOLA')}
+        >
+        </Button>
+        <Image
+          style={styles.imageview}
+          source={require('./assets/movies.png')}
+        />
     </View>
+  
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textview:{
+      color: 'white',
+      padding: 30,
+      textAlign: 'justify',
+      fontWeight: 'bold'
+  },
+  imageview:{
+    justifyContent: 'center',
+    height: 220,
+    width: 220
+  },
+  MainTitle: {
+    alignItems: 'flex-start',
+    fontSize: 20,
+    color: 'white',
+    backgroundColor: 'darkblue'
+  },
+  boton: {
+    color: 'red'
+  }
 });
